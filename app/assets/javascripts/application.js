847cb31113fd2157d48655bbacd86e57448b35c3
shimizu <simichan1976@gmail.com>
2016-10-24 08:49:16 +0000

    underscore.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require underscore
+//= require underscore.js
 //= require gmaps/google
-//= require_tree .
+//= require_tree
